8e646242bd3de51104b4f868da7caf673b271cf5
darkxangel84 <aimeetacchi@hotmail.co.uk>
2018-06-18 23:09:39 +0100

    fix js bug - missing # on query

--- js/script.js
+++ js/script.js
@@ -27,27 +27,31 @@ window.onload = function(){
       .then(function(response){
         return response.json();
       }).then(function(thisJson){
-        console.log(thisJson);
+        console.log(thisJson.types.length);
 
          // Will iterate through the types and put them all into a string 
-        let types = "type";
-        for(let x = 0; x < thisJson.types.length; x++){
-          if(x == thisJson.types.length-1){
+        let types = "";
+        for ( let x = 0; x < thisJson.types.length; x++ ){
+          // if x is equal to 0 --- 1-1 = 0
+          if ( x == thisJson.types.length-1 ){
+            console.log('less than 1');
             types += thisJson.types[x].type.name;
-          }else{
+          } else {
+            console.log('more than 1');
             types += thisJson.types[x].type.name + "-";
           }
         }
-      
+
       // Setting the details of the pokemon to the appropriate fields from response
       const pokemonimage = document.querySelector("#pokemon-img");
       const pokemonname = document.querySelector("#pokemon-details__name");
-      const pokemontype = document.querySelector("pokemon-details__type");
+      const pokemontype = document.querySelector("#pokemon-details__type");
 
       pokemonimage.src = thisJson.sprites.front_default;
       pokemonname.innerText = thisJson.name.toUpperCase();
       pokemontype.innerText = types.toUpperCase();
-   })
+
+   });
 
   });
   
